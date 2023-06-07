03d345599dad683ae8fde319447a5bfe392b0fb9
Marcin Blazejewski <marcinb64@gmail.com>
2023-06-07 07:30:09 -0400

    Build conan lib without cpu & board spec.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class HelloConan(ConanFile):
     name = "liquid"
-    version = "0.2.2"
+    version = "0.2.3"
 
     # Optional metadata
     license = "MIT"
@@ -19,7 +19,7 @@ class HelloConan(ConanFile):
     default_options = {"shared": False, "fPIC": False}
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "CompilerWarnings.cmake", "StaticAnalysis.cmake", "liquid/*", "liquid/src/*", "liquid/src/avr/*", "liquid/src/avr/boards/*", "app/*"
+    exports_sources = "CMakeLists.txt", "CompilerWarnings.cmake", "StaticAnalysis.cmake", "liquid/*", "liquid/src/*", "liquid/src/avr/*", "liquid/src/avr/boards/*"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -34,7 +34,7 @@ class HelloConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(variables={"DAVR_CPU": "atmega168", "F_CPU": "16000000", "LIQUID_PLATFORM": "avr", "LIQUID_BOARD": "ArduinoNano"})
+        cmake.configure(variables={"LIB_ONLY": "True", "LIQUID_PLATFORM": "avr"})
         cmake.build()
 
     def package(self):
